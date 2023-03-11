39f63ac6b3cf490c2f1c4a2d33894f4b5f98cb5f
Elfeiz <tanguy.perot@gmail.com>
2023-03-11 11:27:44 +0100

    I don't remember what I changed

--- Scripts/playercontrol.gd
+++ Scripts/playercontrol.gd
@@ -6,6 +6,7 @@ func _physics_process(delta):
 	var velocity = Vector2()
 
 	if Input.is_action_pressed("ui_down"):
+		
 		velocity.y += 1
 	velocity = velocity.normalized() * speed
 	var collision = move_and_collide(velocity * delta)
