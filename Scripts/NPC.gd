f83eb42e9deb52cbd7d994e9f520601a4f971b68
Elfeiz <tanguy.perot@gmail.com>
2023-03-12 21:38:39 +0100

    First prototype (at least its working)
    Version 0.0.0.0.0.8
    this is pretty much the entire concept of the game, obviously it ain't pretty right now, I will make it prettier once I receive the animations and all that, for now I will just focus on upgrading the gameplay, adding game over screen and all that, will work on the display as well. keep in mind that this is the Player vs Player mode. I will be working on the Player vs AI a bit later.

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -1,15 +1,19 @@
 extends KinematicBody2D
 
-export var speed = 100
+export(int) var bulletSpeed
+var bullet_scene = preload("res://Scenes/bullet2.tscn")
 var isLive=true
-var health=2
+var health=4
 var red_c = true
 var enemy
 var enemy_scene = load("res://Scenes/enemy_scene.tscn")
+var shoot
+
+
 func _physics_process(delta):
 	var velocity = Vector2()
 
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("ui_down2"):
 		if red_c == true:
 			$CollisionShape2D.set_disabled(true)
 			red_c = false
@@ -19,7 +23,11 @@ func _physics_process(delta):
 			red_c = true
 	var collision = move_and_collide(velocity * delta)
 	if !isLive:
-		return 
+		return
+	shoot =Input.is_action_just_pressed("fire2") 
+	if shoot:
+		fire()
+		
 func hit_by_bullet(pos):
 	if !isLive:
 		return
@@ -30,6 +38,11 @@ func hit_by_bullet(pos):
 		queue_free()
 		#removeenemy()
 		
+func fire():
+	var new_bullet=bullet_scene.instance() 
+	new_bullet.position = global_position + Vector2(-40, 0)
+	add_child(new_bullet)
+
 func spawnenemy():
 	enemy = enemy_scene.instance()
 	add_child(enemy)
--- Scripts/playercontrol.gd
+++ Scripts/playercontrol.gd
@@ -3,14 +3,21 @@ extends KinematicBody2D
 var bullet_scene = preload("res://Scenes/bullet.tscn")
 export var speed = 100
 var shoot
+var health = 3
+var isLive=true
+var red_c=true
 
 func _physics_process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_down"):
-		
-		velocity.y += 1
-	velocity = velocity.normalized() * speed
+	if Input.is_action_just_pressed("ui_down"):
+		if red_c == true:
+			$CollisionShape2D.set_disabled(true)
+			red_c = false
+		else:
+			$CollisionShape2D.set_disabled(false)
+			
+			red_c = true
 	var collision = move_and_collide(velocity * delta)
 	shoot =Input.is_action_just_pressed("fire")
 	if shoot:
@@ -20,6 +27,15 @@ func fire():
 	var new_bullet=bullet_scene.instance() 
 	new_bullet.position = global_position + Vector2(40, 0)
 	add_child(new_bullet)
+
+func hit_by_bullet(pos):
+	if !isLive:
+		return
+	health -=1
+	
+	if health<0 :
+		isLive=false
+		queue_free()
 	
 	
 	  
