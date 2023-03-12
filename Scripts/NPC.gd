206e5601b83de16f7600f1d7ef97a02bc9bd25ab
Elfeiz <tanguy.perot@gmail.com>
2023-03-12 17:39:34 +0100

    ENEMY KILLED
    managed to fix it, not sure its the best way to do it but it should do for now

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -4,7 +4,7 @@ export var speed = 100
 var isLive=true
 var health=2
 var enemy
-#var enemy_scene = load("res://Scenes/enemy_scene.tscn").instance()
+var enemy_scene = load("res://Scenes/enemy_scene.tscn")
 func _physics_process(delta):
 	var velocity = Vector2()
 
@@ -15,20 +15,22 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if !isLive:
 		return 
-func hit_by_bullet():
+func hit_by_bullet(pos):
 	if !isLive:
 		return
 	health -=1
 	
 	if health<0 :
 		isLive=false
-#		removeenemy()
+		queue_free()
+		#removeenemy()
 		
-#func spawnenemy():
-#	enemy = enemy_scene.instance()
-#	add_child(enemy)
+func spawnenemy():
+	enemy = enemy_scene.instance()
+	add_child(enemy)
 	
-#func removeenemy():
-#	if enemy: 
-#		enemy.queue_free()
-#	spawnenemy()
+	
+func removeenemy():
+	if enemy: 
+		enemy.queue_free()
+	spawnenemy()
